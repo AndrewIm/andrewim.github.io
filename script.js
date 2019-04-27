0b8939bd76f4ce07e88d817e1cf740f9cb7e4f7a
AndrewIm <imandrew64@gmail.com>
2019-04-26 23:22:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function printout() {
     document.getElementById('first').value + "," +
      document.getElementById('last').value + "," +
       document.getElementById('date').value + "," +
-       document.getElementById('time').value;
+       document.getElementById('time').value+ "," +
+        document.getElementById('comment').value;
           }
 
