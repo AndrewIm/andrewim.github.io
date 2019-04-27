1a741eb8098a3b0d8b395dc1f2bca5e5857fd820
AndrewIm <imandrew64@gmail.com>
2019-04-26 23:29:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function printout() {
+  
+  document.getElementById("print").innerHTML = "CSV Format: "+
+   document.getElementById('number').value + "," +
+    document.getElementById('first').value + "," +
+     document.getElementById('last').value + "," +
+      document.getElementById('date').value + "," +
+       document.getElementById('time').value + "," +
+        document.getElementById('comment').value;
+          }
 
