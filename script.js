bcd17f39c0992c07550602e5ef89883b0f1a87c9
AndrewIm <imandrew64@gmail.com>
2019-04-26 22:04:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+<script>
+function printout() {
+  
+  document.getElementById("print").innerHTML = "CSV Format: "+
+   document.getElementById('number').value + "," +
+    document.getElementById('first').value + "," +
+     document.getElementById('last').value + "," +
+      document.getElementById('date').value + "," +
+       document.getElementById('time').value;
+          }
+</script>
